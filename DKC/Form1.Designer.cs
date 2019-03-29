55487ff21907af9216c317cc47b3fceeb2010ca7
kegan hollern <keganhollern@gmail.com>
2019-03-29 02:58:27 -0400

    Initial Commit

---
+++ DKC/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace DKC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ctrlSteamDir = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ctrlMainAccountId = new System.Windows.Forms.ComboBox();
+            this.ctrlNewAccountId = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ctrlSteamDir
+            // 
+            this.ctrlSteamDir.Location = new System.Drawing.Point(12, 15);
+            this.ctrlSteamDir.Name = "ctrlSteamDir";
+            this.ctrlSteamDir.ReadOnly = true;
+            this.ctrlSteamDir.Size = new System.Drawing.Size(233, 20);
+            this.ctrlSteamDir.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(251, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Steam DIR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ctrlMainAccountId
+            // 
+            this.ctrlMainAccountId.Enabled = false;
+            this.ctrlMainAccountId.FormattingEnabled = true;
+            this.ctrlMainAccountId.Location = new System.Drawing.Point(132, 41);
+            this.ctrlMainAccountId.Name = "ctrlMainAccountId";
+            this.ctrlMainAccountId.Size = new System.Drawing.Size(194, 21);
+            this.ctrlMainAccountId.TabIndex = 2;
+            this.ctrlMainAccountId.SelectedIndexChanged += new System.EventHandler(this.ctrlMainAccountId_SelectedIndexChanged);
+            // 
+            // ctrlNewAccountId
+            // 
+            this.ctrlNewAccountId.Enabled = false;
+            this.ctrlNewAccountId.FormattingEnabled = true;
+            this.ctrlNewAccountId.Location = new System.Drawing.Point(132, 68);
+            this.ctrlNewAccountId.Name = "ctrlNewAccountId";
+            this.ctrlNewAccountId.Size = new System.Drawing.Size(194, 21);
+            this.ctrlNewAccountId.TabIndex = 3;
+            this.ctrlNewAccountId.SelectedIndexChanged += new System.EventHandler(this.ctrlNewAccountId_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Main Acc SteamID 3: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "New Acc SteamID 3: ";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(231, 95);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Copy Keybinds";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Status: Waiting for inputs...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 130);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ctrlNewAccountId);
+            this.Controls.Add(this.ctrlMainAccountId);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ctrlSteamDir);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Dota Keybinds Changer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ctrlSteamDir;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox ctrlMainAccountId;
+        private System.Windows.Forms.ComboBox ctrlNewAccountId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ DKC/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DKC
+{
+    public partial class Form1 : Form
+    {
+        string SteamDir;
+        List<string> account_folder_names = new List<string>();
+        string main_account;
+        string new_account;
+
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                string[] lines = File.ReadAllLines("settings.ini");
+
+                if(lines.Length == 3)
+                {
+                    SteamDir = lines[0];
+                    main_account = lines[1];
+                    new_account = lines[2];
+                    UpdateSteamDir();
+                    int err = 0;
+                    try
+                    {
+                        ctrlMainAccountId.SelectedIndex = ctrlMainAccountId.FindString($"[U:1:{main_account}]");
+                        if(ctrlMainAccountId.SelectedIndex != -1)
+                        {
+                            ctrlNewAccountId.Enabled = true;
+                        }
+                    } catch
+                    {
+                        err++;
+                    }
+                    try
+                    {
+                        ctrlNewAccountId.SelectedIndex = ctrlNewAccountId.FindString($"[U:1:{new_account}]");
+                    }
+                    catch { err++; }
+                    if(err == 0)
+                    {
+                        button2.Enabled = true;
+                    }
+
+                }
+
+            } catch { }
+        }
+        public delegate void SetStatusCallback(string status);
+        public void SetStatus(string status)
+        {
+            if(label1.InvokeRequired)
+            {
+                label1.Invoke(new SetStatusCallback(SetStatus), status);
+                return;
+            }
+            label3.Text = "Status: " + status;
+        }
+        private void UpdateSteamDir()
+        {
+            ctrlSteamDir.Text = SteamDir;
+
+            try
+            {
+                foreach (string dir in Directory.EnumerateDirectories(SteamDir + "userdata"))
+                {
+                    account_folder_names.Add(dir.ToLower().Replace(SteamDir.ToLower() + "userdata\\", ""));
+                }
+
+                ctrlMainAccountId.Items.AddRange(account_folder_names.Select((s) => { return $"[U:1:{s}]"; }).ToArray());
+                ctrlNewAccountId.Items.AddRange(account_folder_names.Select((s) => { return $"[U:1:{s}]"; }).ToArray());
+                ctrlMainAccountId.Enabled = true;
+            }
+            catch
+            {
+                SetStatus("Steam path incorrect?");
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                SteamDir = folderBrowserDialog1.SelectedPath;
+                if (!SteamDir.EndsWith("\\"))
+                    SteamDir = SteamDir + "\\";
+
+                if (SteamDir.EndsWith("\\userdata\\"))
+                    SteamDir = SteamDir.Replace("userdata\\", "");
+
+                UpdateSteamDir();
+            }
+        }
+
+        private void ctrlMainAccountId_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ctrlNewAccountId.Enabled = true;
+            main_account = account_folder_names.ElementAt(ctrlMainAccountId.SelectedIndex);
+        }
+
+        private void ctrlNewAccountId_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            button2.Enabled = true;
+            new_account = account_folder_names.ElementAt(ctrlNewAccountId.SelectedIndex);
+            label3.Text = "Status: waiting to begin...";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label3.Text = "Status: starting keybind transfer...";
+            new Task(() =>
+            {
+
+                string main_folder = SteamDir + @"userdata\" + main_account + @"\570";
+                string new_folder = SteamDir + @"userdata\" + new_account + @"\570";
+
+                if(!Directory.Exists(main_folder))
+                {
+                    SetStatus("Main folder does not exist?");
+                    return;
+                }
+                if (!Directory.Exists(new_folder))
+                {
+                    SetStatus("New folder does not exist?");
+                    return;
+                }
+
+                try
+                {
+
+                    Copy(main_folder, new_folder);
+
+                    File.WriteAllLines("settings.ini", new string[] { SteamDir, main_account, new_account });
+
+                    SetStatus("Keybind copy complete.");
+                } catch
+                {
+
+                    SetStatus("Failed to copy keybinds.");
+                }
+            }).Start();
+        }
+
+
+        public void Copy(string sourceDirectory, string targetDirectory)
+        {
+            DirectoryInfo diSource = new DirectoryInfo(sourceDirectory);
+            DirectoryInfo diTarget = new DirectoryInfo(targetDirectory);
+
+            CopyAll(diSource, diTarget);
+        }
+
+        public void CopyAll(DirectoryInfo source, DirectoryInfo target)
+        {
+            Directory.CreateDirectory(target.FullName);
+
+            // Copy each file into the new directory.
+            foreach (FileInfo fi in source.GetFiles())
+            {
+                SetStatus($"Copying {target.FullName}\\{fi.Name}");
+                fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
+            }
+
+            // Copy each subdirectory using recursion.
+            foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
+            {
+                DirectoryInfo nextTargetSubDir =
+                    target.CreateSubdirectory(diSourceSubDir.Name);
+                CopyAll(diSourceSubDir, nextTargetSubDir);
+            }
+        }
+    }
+}
